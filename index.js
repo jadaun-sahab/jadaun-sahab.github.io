12b04a27bf9d0c62e78030c7a251a0da2f504fc1
vikas jadaun <deadpoolji125@gmail.com>
2023-03-05 12:55:50 +0530

    NEW UPDATE

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* --------------------------------------------------PROJECT------------------------------------------------------ */
+
 let viewmore =document.getElementById("morep");
 let na=document.getElementById("more");
 viewmore.style.display="none"
@@ -11,11 +13,16 @@ function showmore(){
         viewmore.style.display="none"
     }
 }
+
+/* --------------------------------------------------PROJECT------------------------------------------------------ */
+
+
+
+
     // ----------------------------------------dark themee-----------------------------------------
 
     function theme() {
         document.body.classList.toggle("dark-theme")
-        
     }
 
 // ----------------------------------------dark themee-----------------------------------------
@@ -24,6 +31,12 @@ function showmore(){
 
 
 
+
+
+
+
+/* --------------------------------------------------RESUME-DOWNLOAD------------------------------------------------------ */
+
 document.getElementById('resume-link-1').addEventListener("click",()=>{
   window.location.assign("https://drive.google.com/file/d/192ndBi1PBzRkMeaumqFobqZ1BF-GIhAM/view?usp=share_link", "_blank");
 })
@@ -31,16 +44,27 @@ document.getElementById('resume-link-1').addEventListener("click",()=>{
 document.getElementById('resume-link-2').addEventListener("click",()=>{
   window.location.assign("https://drive.google.com/file/d/192ndBi1PBzRkMeaumqFobqZ1BF-GIhAM/view?usp=share_link", "_blank");
 })
-// -------------------------------------------------------------------------------------------
+
+/* --------------------------------------------------RESUME-DOWNLOAD------------------------------------------------------ */
 
 
-    let arr = ["https://github-readme-stats.vercel.app/api/top-langs?username=jadaun-sahab&show_icons=true&locale=en&layout=compact", "https://github-readme-stats.vercel.app/api?username=jadaun-sahab&show_icons=true&locale=en", "https://github-readme-streak-stats.herokuapp.com?user=jadaun-sahab&theme=vue&border_radius=4.3"];
 
-let j=0;
-  function slides2() {
-    document.getElementById("image2").src = arr[j];
-    if (j < arr.length - 1) j++;
-    else j = 0;
+
+
+
+
+
+
+/* --------------------------------------------------GITHUB------------------------------------------------------ */
+
+  let arr = ["https://github-readme-stats.vercel.app/api/top-langs?username=jadaun-sahab&show_icons=true&locale=en&layout=compact", "https://github-readme-stats.vercel.app/api?username=jadaun-sahab&show_icons=true&locale=en", "https://github-readme-streak-stats.herokuapp.com?user=jadaun-sahab&theme=vue&border_radius=4.3"];
+
+    let j=0;
+     function slides2(){
+     document.getElementById("image2").src = arr[j];
+        if (j<arr.length - 1) j++;
+        else j=0;
   }
   setInterval(slides2, 1500);
 
+/* --------------------------------------------------GITHUB------------------------------------------------------ */
